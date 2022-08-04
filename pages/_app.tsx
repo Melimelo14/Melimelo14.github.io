afbc5f3b3668ebf5f499a207e943bbd0ed9cbc91
mathieudutour <mathieu@dutour.me>
2022-08-04 14:59:47 +0200

    add google analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,15 +4,31 @@ import "styles/global.css";
 // core styles shared by all of react-notion-x (required)
 import "react-notion-x/src/styles.css";
 
-// used for collection views selector (optional)
-// TODO: re-add if we enable collection view dropdowns
-// import 'rc-dropdown/assets/index.css'
-
 // global style overrides for notion
 import "styles/notion.css";
 
 import React from "react";
+import Script from "next/script";
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Script
+        strategy="lazyOnload"
+        src={`https://www.googletagmanager.com/gtag/js?id=G-YN521H8YZ9`}
+      />
+
+      <Script strategy="lazyOnload">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', 'G-YN521H8YZ9', {
+            page_path: window.location.pathname,
+          });
+        `}
+      </Script>
+      <Component {...pageProps} />
+    </>
+  );
 }
