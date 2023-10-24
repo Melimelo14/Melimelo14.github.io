863689dd9133eee03d48a5cfaed5692f6a6a6548
mathieudutour <mathieu@dutour.me>
2023-10-24 15:44:36 +0200

    Add 404

--- components/Page404.tsx
+++ components/Page404.tsx
@@ -3,15 +3,27 @@ import * as types from "../lib/types";
 import { SEO } from "./SEO";
 
 import styles from "./styles.module.css";
+import { PageLink } from "./PageLink";
+import { CustomFont } from "./CustomFont";
+import { Header } from "./Header";
 
-export const Page404: React.FC<types.PageProps> = ({ site, pageId, error }) => {
+export const Page404: React.FC<types.PageProps> = ({
+  site,
+  pageId,
+  error,
+  siteMap,
+}) => {
   return (
     <>
-      <SEO site={site} title={site?.name || "Notion Page Not Found"} />
+      <SEO site={site} title={site?.name} />
+
+      <CustomFont site={site} />
+
+      <Header siteMap={siteMap} />
 
       <div className={styles.container}>
         <main className={styles.main}>
-          <h1>Notion Page Not Found</h1>
+          <h1>Page Not Found</h1>
 
           {error ? (
             <p>{error.message}</p>
@@ -23,11 +35,19 @@ export const Page404: React.FC<types.PageProps> = ({ site, pageId, error }) => {
             )
           )}
 
-          <img
+          {/* <img
             src="/404.png"
             alt="404 Not Found"
             className={styles.errorImage}
-          />
+          /> */}
+
+          <PageLink
+            siteMap={siteMap}
+            pageId="Accueil-7f9e6130d76940deb467f36d05b17526"
+            role="menuitem"
+          >
+            Retour à l'Accueil
+          </PageLink>
         </main>
       </div>
     </>
--- pages/404.tsx
+++ pages/404.tsx
@@ -1,3 +1,22 @@
+import { resolveNotionPage } from "../lib/resolve-notion-page";
 import { Page404 } from "../components";
+import { isDev, domain } from "../lib/config";
+
+export const getStaticProps = async () => {
+  try {
+    const props = await resolveNotionPage(domain);
+
+    return {
+      props,
+      revalidate: 10,
+    };
+  } catch (err) {
+    console.error("page error", domain, err);
+
+    // we don't want to publish the error version of this page, so
+    // let next.js know explicitly that incremental SSG failed
+    throw err;
+  }
+};
 
 export default Page404;
