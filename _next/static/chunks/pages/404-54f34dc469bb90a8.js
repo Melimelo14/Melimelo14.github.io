847221bd3697b436fbaf755d0713790e47cf70ab
mathieudutour <mathieudutour@users.noreply.github.com>
2023-11-23 16:20:02 +0000

    deploy: 545ef8ba6add8ecf0882db44dc8d316343b8ec13

---
+++ _next/static/chunks/pages/404-54f34dc469bb90a8.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[197],{6141:function(n,u,_){(window.__NEXT_P=window.__NEXT_P||[]).push(["/404",function(){return _(9114)}])},9114:function(n,u,_){"use strict";_.r(u),_.d(u,{__N_SSG:function(){return c}});var t=_(2654),c=!0;u.default=t.l}},function(n){n.O(0,[479,654,774,888,179],function(){return n(n.s=6141)}),_N_E=n.O()}]);
