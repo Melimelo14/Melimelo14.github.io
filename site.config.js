786215b352e3d8efdb31dd4f7f06b1c7bf2d20f1
Melimelo14 <90412103+Melimelo14@users.noreply.github.com>
2024-10-08 09:28:31 +0200

    Update site.config.js

--- site.config.js
+++ site.config.js
@@ -3,13 +3,13 @@ module.exports = {
   rootNotionPageId: "7f9e6130d76940deb467f36d05b17526",
 
   // basic site info
-  name: "Emilie Meot Médecine Traditionnelle Chinoise",
+  name: "Emilie Meot - Médecine Traditionnelle Chinoise",
   domain: "mtc-passy-mont-blanc.com",
   author: "Emilie Meot",
 
   // open graph metadata (optional)
   description:
-    "Médecine Traditionnelle Chinoise, 995 route du plateau d'Assy, Passy. Acupressure, Massage, Pharmacopée, Diététique. Du bébé à l'adulte, incluant un accompagnement de la femme enceinte.",
+    "Emilie Meot - Médecine Traditionnelle Chinoise, 995 route du plateau d'Assy, Passy. Points d'acupuncture - acupression, Massage, Herboristerie, Diététique. Du bébé à l'adulte, incluant la femme enceinte.",
 
   // social usernames (optional)
   facebook: "mtc.passy.mont.blanc",
